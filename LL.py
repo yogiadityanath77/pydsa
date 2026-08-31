b89f431fdf54623c71d8af584361fba5b8da9c7f
Soham <sohamdheemate711@gmail.com>
2026-09-01 02:45:58 +0530

    Add linked list utility methods: find_max, find_min, count_occurance, insert_after

--- LL.py
+++ LL.py
@@ -29,6 +29,8 @@ class LinkedList:
             self.tail = new_node
         self.length +=1
 
+        return True
+
     def pop(self):
         if self.length == 0:  #no item in list
             return None
@@ -45,8 +47,8 @@ class LinkedList:
         self.length -= 1           # decrement length
 
         if self.length == 0:        # post decrement , if only 1 node was there , remove head and tail
-            self.head == None
-            self.tail == None
+            self.head = None
+            self.tail = None
 
         return temp
             
@@ -71,9 +73,13 @@ class LinkedList:
         self.head = self.head.next
         temp.next = None
 
+        self.length -= 1
+
         if self.length == 0:
             self.tail= None
             self.head= None
+
+        
 
         return temp
 
@@ -115,7 +121,7 @@ class LinkedList:
 
     def remove(self,index):
 
-        if index<0 or index > self.length:
+        if index < 0 or index >= self.length:
             return False
 
         if index == 0:
@@ -133,6 +139,93 @@ class LinkedList:
         self.length -=1
         return temp
 
+    def find_max(self):
+        if self.head == None:   # empty list
+            return None
+
+        maximum = self.head
+        temp = self.head.next   # first node already counted
+
+        while temp is not None:
+            if temp.value > maximum:
+                maximum = temp.value
+
+            temp = temp.next
+
+        return maximum
+
+    def find_min(self):
+
+        if self.head is None:
+            return None
+
+        minimum = self.head
+        temp = self.head.next
+
+        while temp is not None:
+            if temp.value < minimum:
+                minimum = temp.value
+
+            temp = temp.next
+
+        return minimum
+
+    def count_occurance(self,value):
+        count = 0
+        temp = self.head
+        while temp is not None:
+            if temp.value == value:
+                count += 1
+
+            temp = temp.next
+
+        return count
+
+    def insert_after(self, target, value):
+        temp = self.head
+        while temp is not None and temp.value != target:
+            temp = temp.next
+
+        if temp is None :
+            return False
+
+        new_node = Node(value)
+        new_node.next = temp.next
+        temp.next = new_node
+
+        if temp.next == self.tail:
+            self.tail == new_node
+
+        self.length +=1
+        return True
+
+    def delete_occurances(self,value):
+        while self.head is not None and self.head.value == value:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+
+        if self.head is None:
+            self.tail = None
+            return
+
+        prev = self.head 
+        temp = self.head.next
+
+        while temp is not None:
+            if temp.value == value:
+                prev.next = temp.next
+                temp.next = None
+                self.length -= 1
+                temp = prev.next
+            else:
+                prev = temp
+                temp =temp.next
+
+        self.tail = prev
+
+
     def reverse(self):
 
         temp = self.head
@@ -148,5 +241,61 @@ class LinkedList:
             before = temp
             temp = after
 
+    def find_middle_node(self):
 
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def has_loop(self):
+
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+    def find_kth_from_end(self,k):
+
+        slow = self.head
+        fast = self.head
+
+        for _ in range(k):
+
+            if not fast:
+                return None
+
+            fast=fast.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
+
+    def find_kth_from_beginning(self,k):
+        if k < 0 or k > self.length:
+            return None
+
+        temp = self.head
+        for _ in range(k-1):
+            temp = temp.next
+
+        return temp
+
+    
+
+
+    
 
